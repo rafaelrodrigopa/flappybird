626e88321f70f5afd84c629375587176bad15edc
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-06-26 23:06:09 -0300

    Project created

---
+++ android/src/com/rafa/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rafa.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rafa.flappybird.jogo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new jogo(), config);
+	}
+}
