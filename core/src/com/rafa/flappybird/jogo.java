da1331c43dbe9fcd31a5b45bd83cdd2fa91e25dd
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-06-26 23:36:14 -0300

    Configuring SpriteBatch and Texture(bird)

--- core/src/com/rafa/flappybird/jogo.java
+++ core/src/com/rafa/flappybird/jogo.java
@@ -1,20 +1,34 @@
 package com.rafa.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class jogo extends ApplicationAdapter {
-	
+
+//	private int contado = 0;
+
+	private SpriteBatch batch;
+	private Texture passaro;
+
 	@Override
 	public void create () {
-
+//		Gdx.app.log("create","jogo iniciado");
+		batch = new SpriteBatch();
+		passaro = new Texture("passaro1.png");
 	}
 
 	@Override
 	public void render () {
 
+		batch.begin();
+		batch.draw(passaro,300,0);
+		batch.end();
+
+//		contado++;
+//		Gdx.app.log("render","jogo renderizado: " + contado);
 	}
 	
 	@Override
