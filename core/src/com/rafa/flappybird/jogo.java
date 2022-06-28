a83f30610555793f33089d1d2037717047c73e43
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-06-27 21:06:04 -0300

    Moving bird

--- core/src/com/rafa/flappybird/jogo.java
+++ core/src/com/rafa/flappybird/jogo.java
@@ -8,23 +8,39 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class jogo extends ApplicationAdapter {
 
-//	private int contado = 0;
+	private int movimentoX = 0;
+	private int movimentoY = 0;
 
 	private SpriteBatch batch;
 	private Texture passaro;
+	private Texture fundo;
+
+	//Atributos de configurações
+	private float larguraDispositivo;
+	private float alturaDispositivo;
 
 	@Override
 	public void create () {
 //		Gdx.app.log("create","jogo iniciado");
 		batch = new SpriteBatch();
 		passaro = new Texture("passaro1.png");
+		fundo = new Texture("fundo.png");
+
+		larguraDispositivo = Gdx.graphics.getWidth();
+		alturaDispositivo = Gdx.graphics.getHeight();
 	}
 
 	@Override
 	public void render () {
 
 		batch.begin();
-		batch.draw(passaro,300,0);
+
+
+		batch.draw(fundo,0,0,larguraDispositivo,alturaDispositivo);
+		batch.draw(passaro,movimentoX,movimentoY);
+
+		movimentoX++;
+		movimentoY++;
 		batch.end();
 
 //		contado++;
